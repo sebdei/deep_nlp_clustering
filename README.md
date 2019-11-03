12e213edfd676b355c5cc99e646da256b66c1a50
Sebastian Deisel <seb.deisel@posteo.de>
2019-11-03 11:43:54 +0100

    init push with working lstm

---
+++ README.md
@@ -0,0 +1,9 @@
+Start docker 
+
+```docker-compose run python```
+
+In container execute with
+
+```python train.py```
+
+
---
+++ src/preprocess.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from keras.preprocessing.sequence import pad_sequences as keras_pad_sequenecs
+from keras.preprocessing.text import Tokenizer
+
+from model_provider import provide_glove_model
+
+
+def build_embedding_matrix(word_index_dict, vocab_size, feature_dimension_size, model):
+    result = np.zeros((vocab_size, feature_dimension_size))
+    for word, i in word_index_dict.items():
+        embedding_vector = model.get(word)
+        if embedding_vector is not None:
+            result[i] = embedding_vector
+
+    return result
+
+
+def preprocess_word_embedding(sentence_list):
+    tokenizer = Tokenizer()
+    tokenizer.fit_on_texts(sentence_list)
+
+    word_index_dict = tokenizer.word_index
+    vocab_size = len(word_index_dict) + 1  # +1 for zero padding ???
+
+    glove_model = provide_glove_model()
+    feature_dimension_size = 200
+    embedding_matrix = build_embedding_matrix(word_index_dict, vocab_size, feature_dimension_size, glove_model)
+
+    encoded_sequences = tokenizer.texts_to_sequences(sentence_list)
+    padded_sequences = keras_pad_sequenecs(encoded_sequences, padding='post')
+
+    return (embedding_matrix, padded_sequences, vocab_size, feature_dimension_size)
