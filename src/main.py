f0a6b172f822182028806eb667f11e28eb0bbb38
Sebastian Deisel <seb.deisel@posteo.de>
2019-11-22 15:42:12 +0100

    pipeline rdy

--- src/main.py
+++ src/main.py
@@ -1,62 +1,48 @@
-import numpy as np
-from keras.models import Model
+from keras.models import Model, load_model
 from keras.optimizers import SGD
+import pandas as pd
+import numpy as np
+import tensorflow as tf
 
-import clustering
-import lstm_autoencoder
+import clustering_utils
 import preprocess
 import text_provider
 
 NUM_CLUSTERS = 5
 AMOUNT_SEQUENCES = 2225
 
-# sequence_list = text_provider.provide_sequence_list(amount=AMOUNT_SEQUENCES)
 df = text_provider.provide_bbc_sequence_list()
-sequence_list = df.text[0:AMOUNT_SEQUENCES]
-embedding_matrix, padded_sequences = preprocess.preprocess_word_embedding(sequence_list)
+embedding_matrix, padded_sequences = preprocess.preprocess_word_embedding(df.text)
 
-vocab_size = len(embedding_matrix)
-feature_dimension_size = len(embedding_matrix[0])
-max_sequence_length = len(padded_sequences[0])
+# prevents memory issues on GPU
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
 
-print('##################################')
-print('Vocab Size:', vocab_size)
-print('Max Sequence Length:', max_sequence_length)
-print('##################################')
+autoencoder = load_model('models/autoencoder_trained.h5')
 
-autoencoder, encoder = lstm_autoencoder.define_lstm_autoencoder_layers(embedding_matrix, vocab_size, feature_dimension_size, max_sequence_length)
-encoder.summary()
-
-train_expected_autoencoder_output = np.array([[embedding_matrix[word_index] for word_index in encoded_sequence] for encoded_sequence in padded_sequences])
-history = autoencoder.fit(padded_sequences, train_expected_autoencoder_output, epochs=50, verbose=1)
-
-
-# validation_expected_autoencoder_output = np.array([[embedding_matrix[word_index] for word_index in encoded_sequence] for encoded_sequence in validation_padded_sequences])
-# autoencoder.evaluate(validation_padded_sequences, validation_expected_autoencoder_output, verbose=1)
-# Autoencoder was trained - start clustering
+LAST_ENCODER_LAYER_KEY = "last_encoder_layer"
+encoder_output = autoencoder.get_layer(LAST_ENCODER_LAYER_KEY).output
+encoder = Model(inputs=autoencoder.inputs, outputs=encoder_output)
 
 latent_features = encoder.predict(padded_sequences)
-init_cluster_centers = clustering.get_init_kmeans_cluster_centers(NUM_CLUSTERS, latent_features)
+init_cluster_centers = clustering_utils.get_init_kmeans_cluster_centers(NUM_CLUSTERS, latent_features)
 
-clustering_layer = clustering.ClusteringLayer(NUM_CLUSTERS, weights=[init_cluster_centers], name='clustering')(encoder.output)
+clustering_layer = clustering_utils.ClusteringLayer(NUM_CLUSTERS, weights=[init_cluster_centers], name='clustering')(encoder.output)
 encoder_cluster_model = Model(inputs=encoder.input, outputs=clustering_layer)
 encoder_cluster_model.compile(optimizer=SGD(0.01, 0.9), loss='kld')  # Kullback-leibner divergence loss
 
 similarity_scores = encoder_cluster_model.predict(padded_sequences, verbose=0)
-cluster_assignments = clustering.get_cluster_assignments(similarity_scores)
+cluster_assignments = clustering_utils.get_cluster_assignments(similarity_scores)
 
-df['cluster_init'] = cluster_assignments
-# OPTIONAL: Plotting if latent features dimensions = 2
-# plot_scales = utils.get_plot_scales(latent_features)
-# utils.plot_2d_features(features=latent_features, cluster_assignments=cluster_assignments, scales=plot_scales, i='init')
+clusterings_result = pd.DataFrame({'clustering_init': cluster_assignments})
 
 
 # do Soft Assignment Hardening
 
-max_iterations = 60
-update_interval = 6
-batch_size = 20  # wrt to AMOUNT_SEQUENCES!!
-index_array = np.arange(AMOUNT_SEQUENCES)
+max_iterations = 2500
+update_interval = 120
+batch_size = 16  # wrt to AMOUNT_SEQUENCES!!
+index_array = np.arange(len(df.index))
 
 losses = []
 batch_index = 0
@@ -64,10 +50,10 @@ for i in range(int(max_iterations)):
     print("Iteration: %1d / %1d" % (i, max_iterations))
     similarity_scores = encoder_cluster_model.predict(padded_sequences, verbose=1)
     if i % update_interval == 0:
-        target_distribution = clustering.get_target_distribution(similarity_scores)
-        # evaluate the clustering performance
-        # if y is not None:
-        #     acc = np.round(metrics.acc(y, y_pred), 5)
+        target_distribution = clustering_utils.get_target_distribution(similarity_scores)
+    if i % 60:
+        clusterings_result['clustering_'+str(i)] = clustering_utils.get_cluster_assignments(similarity_scores)
+        clusterings_result.to_csv('results/clustering_result.csv')
     idx = index_array[batch_index * batch_size: min((batch_index+1) * batch_size, padded_sequences.shape[0])]
     loss = encoder_cluster_model.train_on_batch(x=padded_sequences[idx], y=target_distribution[idx])
     losses.append(loss)
