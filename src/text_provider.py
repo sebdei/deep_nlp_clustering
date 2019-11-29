ccb126f4ace2052bea2b155492d363fa7866a1b9
k_lim002 <k_lim002@uni-muenster.de>
2019-11-29 19:05:16 +0100

    added train-test split in the BBC function. Unpack format  X_train, X_test, y_train, y_test

--- src/text_provider.py
+++ src/text_provider.py
@@ -1,6 +1,9 @@
 import csv
+import re
 import pandas as pd
+import numpy as np
 from sklearn.datasets import load_files
+from sklearn.model_selection import StratifiedShuffleSplit
 
 
 def provide_sequence_list(amount=-1):
@@ -13,4 +16,11 @@ def provide_sequence_list(amount=-1):
 
 def provide_bbc_sequence_list():
     data = load_files("./data/bbc/", encoding="utf-8", decode_error="replace")
-    return pd.DataFrame(list(zip(data['data'], data['target'])), columns=['text', 'label'])
+    text = np.array([re.sub(r'[^\w\s\n]',"",re.sub("[!.#$%^&*()]","", str(x))).lower().replace('\n','').replace('\\','') for x in data['data']]).reshape(len(data['data']),1)
+    label = data['target']
+    stratSplit = StratifiedShuffleSplit(n_splits=5,test_size=0.4, random_state=42)
+    for train_index, test_index in stratSplit.split(text, label):
+        X_train, X_test = text[train_index], text[test_index]
+        y_train, y_test = label[train_index], label[test_index]
+    
+    return X_train, X_test, y_train, y_test 
