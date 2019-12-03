54dd020a396cd1752a4e1dcc3faef28b4f51b317
Sebastian Deisel <seb.deisel@posteo.de>
2019-12-03 20:02:14 +0100

    Update pretrain_lstm_autoencoder.py

--- src/pretrain_lstm_autoencoder.py
+++ src/pretrain_lstm_autoencoder.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from keras.layers import Dense, Embedding, LSTM, RepeatVector, TimeDistributed
 from keras.models import Sequential
+import tensorflow as tf
 
 import preprocess
 import text_provider
@@ -42,6 +43,9 @@ def define_lstm_autoencoder_layers(embedding_matrix, vocab_size, feature_dimensi
 
 
 def pretrain_lstm_autoencoder(latent_feature_dimensions=32):
+    gpus = tf.config.experimental.list_physical_devices("GPU")
+    tf.config.experimental.set_memory_growth(gpus[0], True)
+
     x_train, x_text, y_train, y_text = text_provider.provide_bbc_sequence_list()
     embedding_matrix, padded_sequences = preprocess.preprocess_word_embedding_fasttext(x_train)
 
@@ -58,7 +62,7 @@ def pretrain_lstm_autoencoder(latent_feature_dimensions=32):
     )
 
     expected_autoencoder_output = np.array([[embedding_matrix[word_index] for word_index in encoded_sequence] for encoded_sequence in padded_sequences])
-    history = autoencoder.fit(padded_sequences, expected_autoencoder_output, epochs=40, verbose=2)
+    history = autoencoder.fit(padded_sequences, expected_autoencoder_output, epochs=40, batch_size=256, verbose=2)
     autoencoder.save(MODEL_PATH + MODEL_BASE_NAME + str(latent_feature_dimensions*2) + "-" + str(latent_feature_dimensions) + ".h5")
     np.save(MODEL_PATH + MODEL_BASE_NAME + str(latent_feature_dimensions*2) + "-" + str(latent_feature_dimensions), history)
     plot_history(history, name=str(latent_feature_dimensions*2) + "-" + str(latent_feature_dimensions))
