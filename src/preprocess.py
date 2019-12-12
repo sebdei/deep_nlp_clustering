72d085d6722fc1b8cf923e5446007aa869efa266
Sebastian Deisel <seb.deisel@posteo.de>
2019-12-12 14:43:58 +0100

    updated glove preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -15,12 +15,12 @@ def build_embedding_matrix(word_index_dict, vocab_size, feature_dimension_size, 
     return result
 
 
-def preprocess_word_embedding(sequence_list):
+def preprocess_word_embedding(sequence_list, label):
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(sequence_list)
 
     word_index_dict = tokenizer.word_index
-    vocab_size = len(word_index_dict) + 1  # +1 for zero padding ???
+    vocab_size = len(word_index_dict) + 1  # +1 for zero padding
 
     glove_model = model_provider.provide_glove_model()
     feature_dimension_size = 200
@@ -29,7 +29,12 @@ def preprocess_word_embedding(sequence_list):
     encoded_sequences = tokenizer.texts_to_sequences(sequence_list)
     padded_sequences = keras_pad_sequenecs(encoded_sequences, padding='post')
 
-    return (embedding_matrix, padded_sequences)
+    stratSplit = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
+    for train_index, test_index in stratSplit.split(padded_sequences, label):
+        x_train, x_test = padded_sequences[train_index], padded_sequences[test_index]
+        y_train, y_test = label[train_index], label[test_index]
+
+    return (embedding_matrix, x_train, x_test, y_train, y_test)
 
 
 def build_embedding_matrix_fasttext(word_index_dict, vocab_size, feature_dimension_size, model):
@@ -48,7 +53,7 @@ def preprocess_word_embedding_fasttext(sequence_list, label):
     tokenizer = Tokenizer()
     tokenizer.fit_on_texts(sequence_list)
     word_index_dict = tokenizer.word_index
-    vocab_size = len(word_index_dict) + 1
+    vocab_size = len(word_index_dict) + 1  # +1 for zero padding
 
     model = model_provider.provide_fasttext_model()
     feature_dimension_size = 300
